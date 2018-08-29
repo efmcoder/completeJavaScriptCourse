696ed9a3d07bf585fbbbf7a5908fc3c80784ec60
efmcoder <faithefmcoder@gmail.com>
2018-08-28 22:50:37 -0400

    variables

--- 2-JS-basics/final/scripttwo.js
+++ 2-JS-basics/final/scripttwo.js
@@ -1,1 +1,21 @@
 console.log("Hello Dear! How are you doing?");
+
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 32;
+
+console.log("His name is " + firstName + " " + lastName + " and he is " + age + " years old");
+
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
