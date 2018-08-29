cc21d75f37f529a56ad009a8abd8c44a37849a68
efmcoder <faithefmcoder@gmail.com>
2018-08-28 22:57:22 -0400

    variables

--- 2-JS-basics/final/scripttwo.js
+++ 2-JS-basics/final/scripttwo.js
@@ -19,3 +19,17 @@ job = 'teacher';
 isMarried = false;
 
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+
+//variable mutation
+age = "twenty eight";
+job = "driver";
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//using a pop-up window instead - use alert
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//using prompt - user input
+//create a variable for the prompt
+var lastName = prompt("What is her last name?");
+console.log(firstName + " " + lastName);
