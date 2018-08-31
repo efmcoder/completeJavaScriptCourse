175aaf7f26b8ecbe8162c16ddaf22698bd888f0b
efmcoder <faithefmcoder@gmail.com>
2018-08-30 22:40:56 -0400

    operators

--- 2-JS-basics/final/scripttwo.js
+++ 2-JS-basics/final/scripttwo.js
@@ -27,9 +27,28 @@ job = "driver";
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
 //using a pop-up window instead - use alert
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+//alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
 //using prompt - user input
 //create a variable for the prompt
-var lastName = prompt("What is her last name?");
+//var lastName = prompt("What is her last name?");
 console.log(firstName + " " + lastName);
+
+var year, yearJohn, yearMark;
+now = 2018;
+johnYOB = 1990;
+markYOB = 1987;
+ageJohn = now - johnYOB;
+ageMark = now - markYOB;
+markOlder = ageMark > ageJohn;
+
+console.log("John is " + ageJohn + " years old " + "and Mark is " + ageMark + " years old");
+console.log(markOlder);
+
+function factorial(number){
+  if(number == 1){
+    return number;
+  } else{
+    return number * factorial(number - 1);
+  }
+}
