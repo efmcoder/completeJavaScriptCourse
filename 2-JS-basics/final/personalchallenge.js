30252ca15f9c3305fcf72db48da37b25c3746b62
efmcoder <faithefmcoder@gmail.com>
2018-10-01 21:39:08 -0400

    objects, functions, methods

--- 2-JS-basics/final/personalchallenge.js
+++ 2-JS-basics/final/personalchallenge.js
@@ -24,3 +24,32 @@ var car = {
 }
 car.findAge();
 console.log(car);
+
+console.log("PART III: YET ANOTHER TRY")
+var me = {
+  firstName: "Faith",
+  lastName: "Chai",
+  birthYear: 1980,
+  findAge: function ()
+  {
+    this.age = 2018 - this.birthYear;
+  }
+}
+me.findAge();
+console.log(me);
+
+console.log("PART IV: 24 HOURS LATER, LET'S SEE IF I REMEMBER HOW TO DO THIS")
+//i) create an object {}
+var brother = {
+  firstName : "James",
+  lastName: "Noah",
+  birthYear: 1982,
+  job: "Engineer",
+
+//ii) add age to brother OBJECT. First create a function
+findAge function(){
+  this.age = 2018 - this.birthYear;
+}
+}
+brother.findAge();
+console.log(brother);
