63e42da9bc64e5afdd5b875eaef3b621c22df016
efmcoder <faithefmcoder@gmail.com>
2018-10-07 15:06:50 -0400

    Loops

--- 2-JS-basics/final/Loops.js
+++ 2-JS-basics/final/Loops.js
@@ -1,4 +1,4 @@
-/*console.log("COUNT UP LOOP");
+console.log("COUNT UP LOOP");
 
 for (var i = 0; i <= 10; i++) {
   console.log(i)
@@ -10,9 +10,9 @@ console.log("COUNT DOWN LOOP");
 //You want to decrease by 1 number at a time, so i--;
 for (var i = 10; i > 0; i--){
   console.log("The number is " + i );
-}*/
+}
 
-var cars = ["BMW", "Volvo", "Mercedes", "Toyota"];
+/*var cars = ["BMW", "Volvo", "Mercedes", "Toyota"];
 //var i;
 //var len = cars.length;
 //for ( i = 0; i < len; i++){ //do not use i-- or u'll have an endless loop
@@ -21,3 +21,4 @@ var cars = ["BMW", "Volvo", "Mercedes", "Toyota"];
 cars.push("Saab");
 console.log(cars);
 }
+*/
