51d9f2a6d44d8911794fa1d6a822e969ebff1bb9
efmcoder <faithefmcoder@gmail.com>
2018-10-07 15:02:25 -0400

    Loops

---
+++ 2-JS-basics/final/Loops.js
@@ -0,0 +1,23 @@
+/*console.log("COUNT UP LOOP");
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i)
+};
+
+console.log("COUNT DOWN LOOP");
+//When you count down, you start from the top, so var i = 10;
+//You want to count & print all numbers greater than 0, so i > 0;
+//You want to decrease by 1 number at a time, so i--;
+for (var i = 10; i > 0; i--){
+  console.log("The number is " + i );
+}*/
+
+var cars = ["BMW", "Volvo", "Mercedes", "Toyota"];
+//var i;
+//var len = cars.length;
+//for ( i = 0; i < len; i++){ //do not use i-- or u'll have an endless loop
+  //console.log(cars);
+
+cars.push("Saab");
+console.log(cars);
+}
