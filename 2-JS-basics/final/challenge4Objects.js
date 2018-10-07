666add624d9b5a8c427a55e5ab199ae43fc98e5b
efmcoder <faithefmcoder@gmail.com>
2018-10-07 14:16:48 -0400

    Objects: Challenge 4

--- 2-JS-basics/final/challenge4Objects.js
+++ 2-JS-basics/final/challenge4Objects.js
@@ -4,14 +4,14 @@ console.log("CODING CHALLENGE 4: BMI CALC")
 //return BMI from the method
 var Mark = {
   fullName: "Mark Chad",
-  weight: 78,
-  height: 1.69,
-  findBMI: function(){
+  weight: 192,
+  height: 1.95,
+  calcBMI: function(){
     this.BMI = Math.round(this.weight/(this.height*this.height));
-    return this.bmi;
+    //return this.bmi; //this seems to have no effect if taken out
   }
 }
-//Mark.findBMIMark();
+//Mark.calcBMIMark();
 //console.log(Mark);
 
 var John = {
@@ -19,23 +19,32 @@ var John = {
   weight: 192,
   height: 1.95,
 
-  findBMI: function(){
+  calcBMI: function(){
     this.BMI = Math.round(this.weight/(this.height*this.height));
-    return this.bmi;
+    //return this.bmi; //this seems to have no effect if taken out
   }
 }
-//John.findBMIJohn(); //this is how we call the function
-//console.log(John);
 
-Mark.findBMI();
-John.findBMI();
+//John.calcBMI(); We can use this. See OPTION 2 BELOW
+//Mark.calcBMI(); We can use this. See OPTION 2 BELOW
+
 console.log(Mark);
 console.log(John);
 
-console.log("HOW TO COMPARE THE BMI's");
+console.log("HOW TO COMPARE THE BMI's - OPTION 1: USING DOT-NOTATION");
 if (Mark.BMI > John.BMI){
   console.log(Mark.fullName + " has a higher BMI of " + Mark.BMI);
 } else if (Mark.BMI < John.BMI) {
   console.log(Mark.fullName + " has a lower BMI of " + Mark.BMI);
+} else{
+  console.log("They have the same BMI");
+};
 
+console.log("OPTION 2: USE CALCULATION RIGHT IN THE COMPARISON STATEMENT");
+if (Mark.calcBMI() > John.calcBMI()){
+  console.log(Mark.fullName + " has a higher BMI of " + Mark.BMI);
+} else if (Mark.calcBMI() < John.calcBMI()) {
+  console.log(Mark.fullName + " has a lower BMI of " + Mark.BMI);
+} else{
+  console.log("They have the same BMI");
 };
